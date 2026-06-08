0590202a9bd2a2d43b6e8e37b5a4cda871cd5aa5
EddS4nchez <edsanchez.laguna@gmail.com>
2026-06-08 14:12:52 -0500

    Update holamondo.py
    Updated the Flask app to use an environment variable for the port.

--- holamondo.py
+++ holamondo.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,4 +9,6 @@ def hola_mundo():
 
 # Ejecutar la aplicación
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    # Esto permite que Railway asigne el puerto automáticamente
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
